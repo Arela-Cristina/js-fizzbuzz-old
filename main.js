7018757022f8970bf552c9a541a3bbec41e2373d
Arela-Cristina <instagrandine@gmail.com>
2024-06-07 10:08:43 +0200

    optimization java script

--- main.js
+++ main.js
@@ -2,32 +2,35 @@ let boxDom = document.getElementById('box');
 
 for (let i = 1; i <= 100; i++) {
 
+    let nuevoDiv = document.createElement("div");
+
     if (i % 3 === 0 && i % 5 === 0) {
-        let nuevoDiv = document.createElement("div");
+        // let nuevoDiv = document.createElement("div");
         nuevoDiv.style.backgroundColor = "red";
         nuevoDiv.textContent = "FizzBuzz";
-        boxDom.append(nuevoDiv);
+        // boxDom.append(nuevoDiv);
 
 
     } else if (i % 3 === 0) {
-        let nuevoDiv = document.createElement("div");
+        // let nuevoDiv = document.createElement("div");
         nuevoDiv.style.backgroundColor = "green";
         nuevoDiv.textContent = "Fizz";
-        boxDom.append(nuevoDiv);
+        // boxDom.append(nuevoDiv);
 
     } else if (i % 5 === 0) {
-        let nuevoDiv = document.createElement("div");
+        // let nuevoDiv = document.createElement("div");
         nuevoDiv.style.backgroundColor = "yellow";
         nuevoDiv.textContent = "Buzz";
-        boxDom.append(nuevoDiv);
+        // boxDom.append(nuevoDiv);
 
     } else {
-        let nuevoDiv = document.createElement("div");
+        // let nuevoDiv = document.createElement("div");
         nuevoDiv.style.backgroundColor = "blue";
         nuevoDiv.textContent = i;
-        boxDom.append(nuevoDiv);
+        // boxDom.append(nuevoDiv);
     }
 
+    boxDom.append(nuevoDiv);
 }
 
 
